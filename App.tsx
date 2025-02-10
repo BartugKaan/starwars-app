de3c89a7ee64b5427b990a8cb1bddb362537a0a4
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-02-10 17:12:02 +0300

    Add Navigation test

--- App.tsx
+++ App.tsx
@@ -1,13 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack'
+
+type RootStackParamList = {
+  Home: undefined
+  Details: { id: number }
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'Details'>
+
+//Navigator Test Content
+const HomeScreen = ({ navigation }: HomeScreenProps) => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Home Screen</Text>
+    <Button
+      title="Go to Details"
+      onPress={() => navigation.navigate('Details', { id: 42 })}
+    ></Button>
+  </View>
+)
+
+const DetailScreen = ({ route }: DetailsScreenProps) => {
+  const { id } = route.params
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Recieved id: {id}</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +54,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
